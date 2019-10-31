2bfc4b5a11dc01e584ae25fc996b05b4ca55f0a5
Uniiiquee <57199637+Uniiiquee@users.noreply.github.com>
2019-10-31 04:02:19 +0100

    Add Swagger support

---
+++ src/main/java/com/github/haappyy/qrcodegenerator/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.github.haappyy.qrcodegenerator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        ApiInfo apiInfo = new ApiInfoBuilder().title("QRCodeGenerator").description("QRCodeGenerator").version("0.1").build();
+        return new Docket(DocumentationType.SWAGGER_2).forCodeGeneration(true).apiInfo(apiInfo)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+}
